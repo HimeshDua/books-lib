16f323afe675acbcfe338a7c458e7fe4e67e4245
Himesh Dua <himeshdua22@gmail.com>
2025-10-19 13:51:55 +0500

    supabase integrated!

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,5 +1,5 @@
-import { createServerClient } from "@supabase/ssr";
-import { cookies } from "next/headers";
+import {createServerClient} from '@supabase/ssr';
+import {cookies} from 'next/headers';
 
 /**
  * Especially important if using Fluid compute: Don't put this client in a
@@ -19,9 +19,7 @@ export async function createClient() {
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options),
-            );
+            cookiesToSet.forEach(({name, value, options}) => cookieStore.set(name, value, options));
           } catch {
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
@@ -29,6 +27,6 @@ export async function createClient() {
           }
         },
       },
-    },
+    }
   );
 }
